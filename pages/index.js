e0c0233be2dabbc1ef66a9acc94fe289c958fbeb
KannagiR <136475740+KannagiR@users.noreply.github.com>
2023-07-05 23:50:48 +0530

    index.js

---
+++ pages/index.js
@@ -0,0 +1,158 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import atm_abi from "../artifacts/contracts/test.sol/test.json";
+
+export default function HomePage() {
+  const [ethWallet, setEthWallet] = useState(undefined);
+  const [account, setAccount] = useState(undefined);
+  const [atm, setATM] = useState(undefined);
+  const [balance, setBalance] = useState(undefined);
+
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const atmABI = atm_abi.abi;
+
+  const getWallet = async () => {
+    if (window.ethereum) {
+      setEthWallet(window.ethereum);
+    }
+
+    if (ethWallet) {
+      const account = await ethWallet.request({ method: "eth_accounts" });
+      handleAccount(account);
+    }
+  };
+
+  const handleAccount = (account) => {
+    if (account) {
+      console.log("Account connected: ", account);
+      setAccount(account);
+    } else {
+      console.log("No account like this is found");
+    }
+  };
+
+  const connectAccount = async () => {
+    if (!ethWallet) {
+      alert("MetaMask wallet is mandatory required to connect");
+      return;
+    }
+
+    const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
+    handleAccount(accounts);
+
+    // once wallet is set we can get a reference to our deployed contract
+    getATMContract();
+  };
+
+  const getATMContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethWallet);
+    const signer = provider.getSigner();
+    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
+
+    setATM(atmContract);
+  };
+
+  const getBalance = async () => {
+    if (atm) {
+      setBalance((await atm.getBalance()).toNumber());
+    }
+  };
+
+  const deposit = async () => {
+    if (atm) {
+      let tx = await atm.deposit(1, { gasLimit: 3e7 });
+      await tx.wait();
+      getBalance();
+    }
+  };
+
+  const withdraw = async () => {
+    if (atm) {
+      let tx = await atm.withdraw(1, { gasLimit: 3e7 });
+      await tx.wait();
+      getBalance();
+    }
+  };
+
+  const initUser = () => {
+    // Check to see if user has Metamask
+    if (!ethWallet) {
+      return <p>You need to install Metamask in order to use this ATM.</p>;
+    }
+
+    // Check to see if user is connected. If not, connect to their account
+    if (!account) {
+      return (
+        <button onClick={connectAccount}>
+          Please connect your Metamask wallet
+        </button>
+      );
+    }
+
+    if (balance == undefined) {
+      getBalance();
+    }
+
+    return (
+      <div class="overlay">
+        <p>Your Balance: {balance}</p>
+        <p>Your Account: {account}</p>
+        <button onClick={deposit}>Deposit 1 ETH</button>
+        <button onClick={withdraw}>Withdraw 1 ETH</button>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    getWallet();
+  }, []);
+
+  return (
+    <main className="container">
+      <header>
+        <h1>WELCOME</h1>
+        <p>what do you want from this atm:</p>
+      </header>
+      {initUser()}
+      <style jsx>
+        {`
+          .container {
+            text-align: center;
+            background-color: red;
+            background-size: cover;
+            color: #fff;
+            font-family: "Times New Roman", serif;
+          }
+
+          header {
+            padding: 30px;
+          }
+
+          h1 {
+            font-family: "Times New Roman", serif;
+            font-size: 80px;
+            margin-bottom: 30px;
+          }
+
+          p {
+            font-size: 18px;
+            margin-bottom: 10px;
+          }
+
+          button {
+            background-color: #4caf50;
+            color: #fff;
+            border: none;
+            padding: 20px 30px;
+            font-size: 16px;
+            cursor: pointer;
+          }
+
+          button:hover {
+            cursor: pointer;
+          }
+        `}
+      </style>
+    </main>
+  );
+}

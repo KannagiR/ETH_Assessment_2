e6583dec79128674a94a65163ec520f35c121e76
KannagiR <136475740+KannagiR@users.noreply.github.com>
2023-07-06 20:47:32 +0530

    deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,11 +8,13 @@ const hre = require("hardhat");
 
 async function main() {
   const initBalance = 1;
-  const Assessment = await hre.ethers.getContractFactory("Assessment");
+  const Assessment = await hre.ethers.getContractFactory("test");
   const assessment = await Assessment.deploy(initBalance);
   await assessment.deployed();
 
-  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address} kumkum sharma`);
+  console.log(
+    `A contract with balance of ${initBalance} eth deployed to ${assessment.address}`
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -21,3 +23,4 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
